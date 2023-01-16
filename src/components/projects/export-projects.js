ab7049b8741717bfa2b24c1c824f386756eff095
karo <karolina.shi@outlook.com>
2023-01-16 16:13:41 +0100

    Updates

--- src/components/projects/export-projects.js
+++
@@ -1,6 +0,0 @@
-import DatatonismPage from './DatatonismPage';
-import SpirPage from './SpirPage';
-import SteammindPage from './SteammindPage'
-import SfbsPage from './SfbsPage'
-
-export {DatatonismPage, SpirPage, SteammindPage, SfbsPage};
