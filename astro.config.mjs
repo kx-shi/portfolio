34f8b7007499da9053a33de334e7c492c1e21beb
Karolina S <karolina.shi@outlook.com>
2026-07-09 22:56:33 +0200

    Add Github actions for deployment of site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'kx-shi.github.io/',
+    base: '/portfolio',
+});
